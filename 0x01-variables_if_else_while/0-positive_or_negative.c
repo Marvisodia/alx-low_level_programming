65cce9c8a13ac8d12847701f1079a25e50f35332
Marvisodia <marvis3798@gmail.com>
2023-10-06 15:45:14 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'Fetch the last digit of a number'
- * Return: Always 0
+ * main -Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int B;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	B = n % 10;
-	if (B > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, B);
-	}
-	else if (B == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, B);
-	}
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, B);
-	}
-	return (0);
+		printf("%d is negative\n", n);
+	return (0);
 }
