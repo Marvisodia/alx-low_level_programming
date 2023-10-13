a5442800cf72f3840c3a9d62b4b326d0fc4915d0
Marvisodia <marvis3798@gmail.com>
2023-10-13 15:57:41 -0700

    6th task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - printing more func
+ */
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m >= 10)
+				_putchar('1');
+			_putchar (m % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
